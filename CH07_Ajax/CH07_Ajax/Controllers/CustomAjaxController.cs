b6317e744c66afa2f8a96dfb3d4212395bf5d536
Tony Chen <tonychenn@gmail.com>
2015-10-05 21:59:27 +0800

    完成第一個demo, 可以用ajax取得partial view.

---
+++ CH07_Ajax/CH07_Ajax/Controllers/CustomAjaxController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CH07_Pratice.Controllers
+{
+    public class CustomAjaxController : Controller
+    {
+        // GET: CustomAjax
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PrivacyPolicy()
+        {
+            if (Request.IsAjaxRequest())
+                return PartialView();
+
+            return View();
+
+        }
+    }
+}
---
+++ CH07_Ajax/CH07_Ajax/Scripts/AjaxDemo.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function() {
+    $("#privacyLink").click(function() {
+        event.preventDefault();
+        var url = $(this).attr('href');
+
+        $('#privacy').load(url);
+    });
+})
